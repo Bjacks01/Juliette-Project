9f0cdce3ce08da2c07b708557dac48166d400280
Bjacks01 <142854037+Bjacks01@users.noreply.github.com>
2023-11-30 16:09:18 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+
+function iFrameToggler() {
+    var iFrameStatus = document.getElementById('content2');
+    if (iFrameStatus.style.display ==='none') {
+        iFrameStatus.style.display = 'block';
+    } else {
+        iFrameStatus.style.display ='none';
+    }
+}
